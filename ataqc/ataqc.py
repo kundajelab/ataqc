e00521077802ff87278a00398bc36687dc635e75
Chris Teng <cteng747@gmail.com>
2017-08-23 20:11:26 -0700

    Final version of my refactor

--- ataqc/ataqc.py
+++ ataqc/ataqc.py
@@ -25,30 +25,36 @@ def parse_args():
     # where to put things
     parser.add_argument('--outprefix', help='Output prefix')
     
+    # run information
+    parser.add_argument('--genome', help='Genome build used')
+    parser.add_argument('--sample_name', help='Sample name')
+
+    # annotation files
+    parser.add_argument('--chromsizes', help='chromsizes file')
+    parser.add_argument('--ref', help='Reference fasta file')
+    parser.add_argument('--tss', help='TSS file')
+    parser.add_argument('--dnase', help='Open chromatin region file')
+    parser.add_argument('--blacklist', help='Blacklisted region file')
+    parser.add_argument('--prom', help='Promoter region file')
+    parser.add_argument('--enh', help='Enhancer region file')
+    parser.add_argument('--reg2map', help='file with cell type signals')
+    parser.add_argument('--meta', help='Roadmap metadata')
+
     # put in processed files
-    parser.add_argument('--fastq1',
-                        help='First set of reads if paired end, \
-                              or the single end reads')
-    parser.add_argument('--fastq2',
-                        help='Second set of reads if paired end')
+    parser.add_argument('--fastq1', help='First set of reads if paired end, or the single end reads')
+    parser.add_argument('--fastq2', help='Second set of reads if paired end')
     parser.add_argument('--alignedbam', help='BAM file from the aligner')
     parser.add_argument('--alignmentlog', help='Alignment log')
     parser.add_argument('--coordsortbam', help='BAM file sorted by coordinate')
     parser.add_argument('--duplog', help='Picard duplicate metrics file')
     parser.add_argument('--pbc', help='ENCODE library complexity metrics file')
     parser.add_argument('--finalbam', help='Final filtered BAM file')
-    parser.add_argument('--finalbed',
-                        help='Final filtered alignments in BED format')
-    parser.add_argument('--bigwig',
-                        help='Final bigwig')
-    parser.add_argument('--peaks',
-                        help='Peak file')
-    parser.add_argument('--naive_overlap_peaks',
-                        default=None, help='Naive overlap peak file')
-    parser.add_argument('--idr_peaks',
-                        default=None, help='IDR peak file')
-    parser.add_argument('--use_sambamba_markdup', action='store_true',
-                        help='Use sambamba markdup instead of Picard')
+    parser.add_argument('--finalbed', help='Final filtered alignments in BED format')
+    parser.add_argument('--bigwig', help='Final bigwig')
+    parser.add_argument('--peaks', help='Peak file')
+    parser.add_argument('--naive_overlap_peaks', default=None, help='Naive overlap peak file')
+    parser.add_argument('--idr_peaks', default=None, help='IDR peak file')
+    parser.add_argument('--use_sambamba_markdup', action='store_true', help='Use sambamba markdup instead of Picard')
 
     # OR put in spreadsheet with all input files
     # NOTE: make sure to allow clean version if someone puts in Excel sheet
@@ -78,14 +84,49 @@ def run_ataqc(args):
     os.system('mkdir -p {}'.format(os.path.dirname(args.outprefix)))
     start = timeit.default_timer()
 
+    # insert unicode-to-byte function here. json.load() output is in unicode format, and is not usable
+    # by samtools or picard
+
     # set up species file
-    with open(args.species, 'r') as fp:
-        species_files = json.load(fp)
+    if (args.species is not None):
+        with open(args.species, 'r') as fp:
+            species_files = json.load(fp)
+        # convert from unicode to byte here
+
+    else:
+        annotations = [args.dnase, args.blacklist, args.prom, args.enh, args.peaks]
+        annotation_names = ['DNAse', 'Blacklist', 'Promoters', 'Enhancers', 'Peaks']
+
+        species_files = {'chrsz': args.chromsizes,
+                         'ref_fa': args.ref,
+                         'annotations': annotations,
+                         'annotation_names': annotation_names,
+                         'tss_enrich': args.tss,
+                         'reg2map': args.reg2map,
+                         'roadmap_meta': args.meta
+        }
 
     # set up inputs file
-    if args.files:
+    if (args.files is not None):
         with open(args.files, 'r') as fp:
             data_files = json.load(fp)
+
+        # convert from unicode to byte here
+    else:
+        peaks = [args.peaks, args.naive_overlap_peaks, args.idr_peaks]
+        peak_names = ['Raw Peaks', 'Naive Overlap Peaks', 'IDR Peaks']
+
+        data_files = {'genome': args.genome,
+                     'sample_name': args.sample_name,
+                     'final_reads_bed': args.finalbed,
+                     'peaks': peaks,
+                     'peak_names': peak_names,
+                     'raw_bam': args.alignedbam,
+                     'final_bam': args.finalbam,
+                     'dup_log': args.duplog,
+                     'pbc_log': args.pbc
+        }
+
 
     metrics = {} # easy to convert this into json or any other format
             
@@ -141,12 +182,12 @@ def run_ataqc(args):
     return qc_groups
 
 
-def viz_ataqc(qc_groups, outprefix):
+def viz_ataqc(qc_groups, sample_name, outprefix):
     """View results - html or multiQC"""
 
     # give option of native html
 
-    viz.write_html(qc_groups, outprefix)
+    viz.write_html(qc_groups, sample_name, outprefix)
 
     # also give option of multiQC
     
@@ -158,7 +199,7 @@ def main():
 
     args = parse_args()
     qc_groups = run_ataqc(args)
-    viz_ataqc(qc_groups, args.outprefix)
+    viz_ataqc(qc_groups, args.sample_name, args.outprefix)
 
 
 
